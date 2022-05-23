4588e4586c9c2bdb241303b628724386c032cd25
Jakob Hubbard <jakobhubbard778@gmail.com>
2022-05-22 21:32:49 -0400

    Change Billboard Attributes
    Had to resize images to be a power of 2 image, and because of that all the sizes and aspects had to be changed to accommodate for the change.  Removed a couple really long group images from being in the bleachers as well.

--- BleacherRandomizer/Billboard.cs
+++ BleacherRandomizer/Billboard.cs
@@ -12,21 +12,19 @@ namespace BleacherRandomizer
         public static void Add_All_Billboards() {
             // Adds 1-6.png as they all have same aspect ratio and size
             for (int i = 0; i < 6; i++) {
-                Add_Billboard(7, 1, (i + 1).ToString() + ".png");
+                Add_Billboard(6, 1, (i + 1).ToString() + ".png");
             }
-            Add_Billboard(8, 0.7, "7.png");
-            Add_Billboard(7, 0.7, "8.png");
-            Add_Billboard(7, 0.5, "9.png");
-            Add_Billboard(8, 1, "dude.png");
-            Add_Billboard(9, 1, "g1.png");
+            Add_Billboard(7.5, 1, "7.png");
+            Add_Billboard(7, 1, "8.png");
+            Add_Billboard(7, 1, "9.png");
+            Add_Billboard(7, 1, "dude.png");
+            Add_Billboard(7, 2, "g1.png");
             Add_Billboard(9, 1, "g2.png");
-            Add_Billboard(9, 1, "g3.png");
-            Add_Billboard(9, 1, "g4.png");
-            Add_Billboard(7, 1.5, "g5.png");
-            Add_Billboard(11, 1, "g6.png");
-            Add_Billboard(9, 1, "groupsit.png");
-            Add_Billboard(7, 0.5, "rogan.png");
-            Add_Billboard(7, 0.5, "rogan2.png");
+            Add_Billboard(8, 1, "g5.png");
+            Add_Billboard(7, 1, "g6.png");
+            Add_Billboard(8, 1, "groupsit.png");
+            Add_Billboard(7, 1, "rogan.png");
+            Add_Billboard(7, 1, "rogan2.png");
         }
 
         private static void Add_Billboard(double size, double aspect, string png) {
